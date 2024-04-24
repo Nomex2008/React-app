9252bb60f7d74d482adf3598b5fcb0fa4ebb15da
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2024-04-24 21:06:37 +0300

    Commit 5 (2)
    Add redax toolkit.

--- lesson-2/my-app/src/store/reducers/ActionCreators.tsx
+++ lesson-2/my-app/src/store/reducers/ActionCreators.tsx
@@ -1,9 +1,10 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { IUser } from "../../models/IUser";
 import { AppDispatch } from "../store";
 import { userSlice } from "./UserSlice";
 
-
+/*
 export const fetchUsers = () => async (dispatch: AppDispatch) => {
     try {
         dispatch(userSlice.actions.usersFetching())
@@ -13,3 +14,16 @@ export const fetchUsers = () => async (dispatch: AppDispatch) => {
         dispatch(userSlice.actions.usersFetchingError(e.massage))
     }
 }
+*/
+
+export const fetchUsers = createAsyncThunk(
+    'user/fetshAll',
+    async (_, thunkAPI) => {
+        try {
+            const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+            return response.data
+        } catch (e) {
+            return thunkAPI.rejectWithValue('Error dont have any user.')
+        }
+    }
+)
--- lesson-2/my-app/src/store/reducers/UserSlice.ts
+++ lesson-2/my-app/src/store/reducers/UserSlice.ts
@@ -1,6 +1,6 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction, ActionReducerMapBuilder } from "@reduxjs/toolkit";
 import { IUser } from "../../models/IUser";
-
+import { fetchUsers } from "./ActionCreators";
 
 interface UserState {
     users: IUser[];
@@ -18,19 +18,22 @@ export const userSlice = createSlice ({
     name: 'user',
     initialState,
     reducers: {
-        usersFetching(state) {
-            state.isLoading = true;
-        },
-        usersFetchingSuccess(state, action: PayloadAction<IUser[]>) {
-            state.isLoading = false;
-            state.error = '';
-            state.users = action.payload
-        },
-        usersFetchingError(state, action: PayloadAction<string>) {
-            state.isLoading = false;
-            state.error = action.payload
-        }
-    }
+    },
+    extraReducers: (builder: ActionReducerMapBuilder<UserState>) => {
+        builder
+            .addCase(fetchUsers.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = '';
+                state.users = action.payload;
+            })
+            .addCase(fetchUsers.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(fetchUsers.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload ? action.payload.toString() : 'Unknown error';
+            });
+    },
 })
 
 export default userSlice.reducer;
