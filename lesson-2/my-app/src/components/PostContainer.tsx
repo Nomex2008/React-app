93aaf1738a6e804b0f1852b3cbc850e14eb48ec4
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2024-05-09 00:38:43 +0300

    Commit 9 (2)
    Add deletePost and updatePost.

--- lesson-2/my-app/src/components/PostContainer.tsx
+++ lesson-2/my-app/src/components/PostContainer.tsx
@@ -6,12 +6,22 @@ import PostItem from "./PostItem";
 const PostContainer = ({}) => {
     const {data: posts, error, isLoading} = postAPI.useFetchAllPostsQuery(100)
     const [createPost, {}] = postAPI.useCreatePostMutation()
+    const [deletePost, {}] = postAPI.useDeletePostMutation()
+    const [updatePost, {}] = postAPI.useUpdatePostMutation()
 
     const hendleCreate = async () => {
         const title = prompt()
         const numberOfPosts = posts?.length || Date.now()
-        const id = numberOfPosts + 1
+        const id = `new ${numberOfPosts + 1}`
         await createPost({title, id, body: 'author'} as any)
+    }
+
+    const handleRemove = (post: IPost) => {
+        deletePost(post)
+    }
+
+    const handleUpdate = (post: IPost) => {
+        updatePost(post)
     }
 
     return (
@@ -21,7 +31,7 @@ const PostContainer = ({}) => {
                 {isLoading && <h1>Loading...</h1>}
                 {error && <h1>ERROR</h1>}
                 {posts && posts.map(post => 
-                    <PostItem key = {post.id} post = {post}/>
+                    <PostItem remove={handleRemove} update={handleUpdate} key = {post.id} post = {post}/>
                 )}
             </div>
         </div>
